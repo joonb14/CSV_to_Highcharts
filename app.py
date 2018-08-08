da20e9b8ae693dcd6d01775a070a5311779009d4
joonb14 <joonb14@gmail.com>
2018-08-09 04:25:31 +0900

    lots of changes

--- app.py
+++ app.py
@@ -6,32 +6,10 @@ import json
  
 app = Flask(__name__)
 
-@app.route('/about')
-def aboutpage():
-
-    title = "About this site"
-    paragraph = ["blah blah blah memememememmeme blah blah memememe"]
-
-    pageType = 'about'
-
-    return render_template("index.html", title=title, paragraph=paragraph, pageType=pageType)
-
-
-@app.route('/about/contact')
-def contactPage():
-
-    title = "About this site"
-    paragraph = ["blah blah blah memememememmeme blah blah memememe"]
-
-    pageType = 'about'
-
-    return render_template("index.html", title=title, paragraph=paragraph, pageType=pageType)
-
-
 @app.route('/')
 @app.route('/graph')
 def graph(chart_type = 'line', chart_height = 500):
-    files = glob.glob("*-*-*_*-*-*.csv")
+    files = glob.glob("*.csv")
     df=pd.read_csv(files[0], names=['Name','UUID','Major','Minor','formattedtime','time','temperature','humidity','rssi','data'])
     #Major over 10 is not the packet we want
     df=df[(df.Major < 10)]
@@ -50,7 +28,6 @@ def graph(chart_type = 'line', chart_height = 500):
 
         chartInfo.append([chartID, chart, series, title, xAxis, yAxis])
 
-
     return render_template('index.html', chartInfo=chartInfo)
 
 def getSeries(df,option):
@@ -62,11 +39,9 @@ def getSeries(df,option):
         series += '{"name":"Major ' + str(major) + '","data":['
         for index, row in df.iterrows():
             if row['Major'] == major:
-                series += '[' + str(row['time']) + ',' + str(int(row[option])) + '],'
-                   
+                series += '[' + str(row['time']) + ',' + str(int(row[option])) + '],'                   
         series = series[:-1]
         series += ']},'
-
     series = series[:-1] + ']'
 
     return series
